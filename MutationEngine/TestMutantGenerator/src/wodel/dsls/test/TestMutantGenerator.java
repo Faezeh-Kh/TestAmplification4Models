0d70a506b9c6ffb5da32d33b4d8611a642c340e3
Pablo Gómez-Abajo <gomezabajo@yahoo.com>
2023-02-24 16:26:55 +0100

    mutant generator @Faezeh-Kh

---
+++ MutationEngine/TestMutantGenerator/src/wodel/dsls/test/TestMutantGenerator.java
@@ -0,0 +1,31 @@
+package wodel.dsls.test;
+
+import wodel.dsls.WodelUtils;
+
+import java.io.IOException;
+
+import exceptions.MetaModelNotFoundException;
+import mutatorenvironment.MutatorenvironmentPackage;
+
+public class TestMutantGenerator {
+	public static void main(String[] args) {
+		
+		MutatorenvironmentPackage.eINSTANCE.getClass();
+
+		try {
+			WodelUtils.generateMutationOperators(new String[] {"c:/GemocStudio/workspace2/wodeleduapp/data/model/DFAAutomaton.ecore", "d:/dfa/models", "c:/GemocStudio/workspace2/wodeleduapp"});
+		} catch (MetaModelNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+
+		//WodelUtils.compileWodelProject(new String[] {"c:/GemocStudio/workspace2/wodeleduapp", "c:/GemocStudio", "GemocStudioc"});
+		
+		WodelUtils.generateMutants(new String[]{"d:/dfa/models", "d:/dfa/mutants", "c:/GemocStudio/workspace2/wodeleduapp", "c:/GemocStudio"});
+	}
+}
