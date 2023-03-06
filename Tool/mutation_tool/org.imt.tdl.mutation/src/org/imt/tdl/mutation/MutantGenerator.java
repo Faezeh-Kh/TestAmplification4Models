d5c2c6904a3b2c8d9aac17c38487c040c1621f19
Faezeh Khorram <khorram.faezeh@gmail.com>
2023-03-06 12:25:09 +0100

    add the jars and plugins needed for mutation operator generator

--- Tool/mutation_tool/org.imt.tdl.mutation/src/org/imt/tdl/mutation/MutantGenerator.java
+++ Tool/mutation_tool/org.imt.tdl.mutation/src/org/imt/tdl/mutation/MutantGenerator.java
@@ -1,4 +1,7 @@
 package org.imt.tdl.mutation;
+import java.io.IOException;
+
+import exceptions.MetaModelNotFoundException;
 import wodel.dsls.WodelUtils;
 
 /**
@@ -7,11 +10,23 @@ import wodel.dsls.WodelUtils;
  */
 public class MutantGenerator {
 	public static void main(String[] args) {
+		String wodelProjectPath = "c:/labtop/gemoc_studio/workspaces/xTDL_Amplification/ArduinoMutation";
+		String metamodelPath = wodelProjectPath + "/data/model/arduino.ecore";
+		String inputPath = "";
+		String eclipseHomePath = "c:/labtop/gemoc_studio";
+		String eclipseCompilerName = "GemocStudioc";
+		String outputPath = wodelProjectPath + "/out/mutants";
 		
-		WodelUtils.generateMutants(new String[]{
-				"c:/labtop/gemoc_studio/workspaces/xTDL_Amplification/mutationOperatorGenerator/models", 
-				"c:/labtop/gemoc_studio/workspaces/xTDL_Amplification/mutationOperatorGenerator/mutants", 
-				"c:/GemocStudio/workspace2/wodeleduapp", 
-				"c:/GemocStudio"});
+		try {
+			WodelUtils.generateMutationOperators(new String[] {metamodelPath, inputPath, wodelProjectPath});
+			//WodelUtils.compileWodelProject(new String[] {wodelProjectPath, eclipseHomePath, eclipseCompilerName});
+			//WodelUtils.generateMutants(new String[] {inputPath, outputPath, wodelProjectPath, eclipseHomePath});
+		} catch (MetaModelNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
