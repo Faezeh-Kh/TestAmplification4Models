e5c02441a382a6df87533ce69159829f75c9d994
Pablo Gómez-Abajo <gomezabajo@yahoo.com>
2023-03-13 19:53:54 +0100

    compatibility with java 11 -- @Faezeh-Kh

--- MutationEngine/TestMutantGenerator/src/wodel/dsls/test/TestMutantGenerator.java
+++ MutationEngine/TestMutantGenerator/src/wodel/dsls/test/TestMutantGenerator.java
@@ -4,16 +4,15 @@ import wodel.dsls.WodelUtils;
 
 import java.io.IOException;
 
-import exceptions.MetaModelNotFoundException;
+import wodel.utils.exceptions.MetaModelNotFoundException;
 import mutatorenvironment.MutatorenvironmentPackage;
 
 public class TestMutantGenerator {
 	public static void main(String[] args) {
 		
 		MutatorenvironmentPackage.eINSTANCE.getClass();
-		/*
 		try {
-			WodelUtils.generateMutationOperators(new String[] {"c:/GemocStudio/workspace2/wodeleduapp/data/model/DFAAutomaton.ecore", "d:/dfa/models", "c:/GemocStudio/workspace2/wodeleduapp"});
+			WodelUtils.generateMutationOperators(new String[] {"c:/GemocStudio/workspace2/wodeldfa/data/model/DFAAutomaton.ecore", "d:/dfa/models", "c:/GemocStudio/workspace2/wodeldfa"});
 		} catch (MetaModelNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -23,9 +22,8 @@ public class TestMutantGenerator {
 			e.printStackTrace();
 			return;
 		}
-		*/
-		//WodelUtils.compileWodelProject(new String[] {"c:/GemocStudio/workspace2/wodeleduapp", "c:/GemocStudio", "GemocStudioc"});
+		WodelUtils.compileWodelProject(new String[] {"c:/GemocStudio/workspace2/wodeldfa", "c:/GemocStudio", "GemocStudioc"});
 		
-		WodelUtils.generateMutants(new String[]{"d:/dfa/models", "d:/dfa/mutants", "c:/GemocStudio/workspace2/wodeleduapp", "c:/GemocStudio"});
+		WodelUtils.generateMutants(new String[]{"d:/dfa/models", "d:/dfa/mutants", "c:/GemocStudio/workspace2/wodeldfa", "c:/GemocStudio"});
 	}
 }
--- MutationEngine/TestMutantGeneratorStatemachines/src/wodel/dsls/test/TestMutantGenerator.java
+++ MutationEngine/TestMutantGeneratorStatemachines/src/wodel/dsls/test/TestMutantGenerator.java
@@ -4,16 +4,15 @@ import wodel.dsls.WodelUtils;
 
 import java.io.IOException;
 
-import exceptions.MetaModelNotFoundException;
+import wodel.utils.exceptions.MetaModelNotFoundException;
 import mutatorenvironment.MutatorenvironmentPackage;
 
 public class TestMutantGenerator {
 	public static void main(String[] args) {
 		
 		MutatorenvironmentPackage.eINSTANCE.getClass();
-		/*
 		try {
-			WodelUtils.generateMutationOperators(new String[] {"c:/GemocStudio/workspace2/wodelsm/data/model/statemachines.ecore", "d:/sm/models", "c:/GemocStudio/workspace2/wodelsm"});
+			WodelUtils.generateMutationOperators(new String[] {"c:/GemocStudio/workspace2/wodelsm/data/model/statemachines.ecore", "d:/sm/empty", "c:/GemocStudio/workspace2/wodelsm"});
 		} catch (MetaModelNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -23,8 +22,7 @@ public class TestMutantGenerator {
 			e.printStackTrace();
 			return;
 		}
-		*/
-		//WodelUtils.compileWodelProject(new String[] {"c:/GemocStudio/workspace2/wodelsm", "c:/GemocStudio", "GemocStudioc"});
+		WodelUtils.compileWodelProject(new String[] {"c:/GemocStudio/workspace2/wodelsm", "c:/GemocStudio", "GemocStudioc"});
 		
 		WodelUtils.generateMutants(new String[]{"d:/sm/models", "d:/sm/mutants", "c:/GemocStudio/workspace2/wodelsm", "c:/GemocStudio"});
 	}
